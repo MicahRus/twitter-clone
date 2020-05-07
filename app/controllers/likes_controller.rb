b76f9eb1fef780e285016fa2e1275c9c3ef1d841
Micah <Rus.Micah@gmail.com>
2020-05-07 13:53:12 +1000

    Changed rubocop yaml file to not force method lengths

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,20 +3,15 @@ class LikesController < ApplicationController
     @tweet = params[:tweet_id]
     @user = current_user.id
     likes = { user_id: @user, tweet_id: @tweet, has_liked: true }
-
     @likes = Like.all
     @like = Like.new(likes)
-
     @likes.each do |x|
       if x.user_id == @like.user_id && x.tweet_id == @like.tweet_id
         x.destroy
         @like.destroy
         break
       end
-
-    @like.save!
+      @like.save!
     end
-
   end
-
 end
