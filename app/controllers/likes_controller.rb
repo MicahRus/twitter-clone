426aeb1b73fde3e1b7b0768dc942684ba970be05
Micah <Rus.Micah@gmail.com>
2020-05-07 10:19:12 +1000

    rubocopped

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,15 @@
 class LikesController < ApplicationController
-
   def create
     @tweet = params[:tweet_id]
     @user = current_user.id
-    likes = {user_id: @user, tweet_id: @tweet, has_liked: true}
+    likes = { user_id: @user, tweet_id: @tweet, has_liked: true }
     @like = Like.create(likes)
-    
+
     @like.save!
-    if @like.save 
+    if @like.save
       redirect_to user_path(@user)
-      puts "*" * 25
-      puts "Like succeeded"
-      puts "*" * 25
     else
       redirect_to tweet_path(@tweet)
-      puts "*" * 25
-      puts "Like did not succeed"
-      puts "*" * 25
     end
-  
-
   end
-
 end
